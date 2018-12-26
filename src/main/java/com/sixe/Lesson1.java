b4f5265f1337055c080974420511e9b155d1fb61
quanzhen <fquanzhen@gmail.com>
2018-12-26 23:31:24 +0800

    init

---
+++ src/main/java/com/sixe/Lesson1.java
@@ -0,0 +1,24 @@
+package com.sixe;
+import com.alibaba.fastjson.JSON;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Lesson1 {
+	public static void main(String args[]) {
+		HashSet<Integer> hash = new HashSet<Integer>();
+		hash.add(1);
+		hash.add(2);
+		hash.add(3);
+		hash.add(4);
+		hash.add(5);
+		hash.remove(3);
+		Iterator<Integer> itr = hash.iterator();
+		while (itr.hasNext()) {
+			Integer str = itr.next();
+			System.out.println(str);
+		}
+		System.out.println(hash);
+		System.out.println(hash.size());
+	
+	}
+}
