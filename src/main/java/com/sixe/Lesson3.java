e8ff45ddcdf60d0e682a311512d12f4e5a01cf57
quanzhen <fquanzhen@gmail.com>
2018-12-28 17:36:19 +0800

    add new file Lesson3 and Lesson4

--- src/main/java/com/sixe/Lesson3.java
+++ src/main/java/com/sixe/Lesson3.java
@@ -6,20 +6,20 @@ import java.util.Map;
 
 public class Lesson3 {
 	public static void main(String args[]) {
-	HashMap<String, Integer> map = new HashMap<String, Integer>();
-	map.put("li",1);
-	map.put("wang",2);
-	map.put("sun",3);
-	map.put("zhao",4);
-	map.put("xie",5);
-	map.remove("sun");
-	Iterator<Map.Entry<String, Integer>> entries = map.entrySet().iterator();	 
-	while (entries.hasNext()) {	 
-	    Map.Entry<String, Integer> entry = entries.next();	 
-	    System.out.println( entry.getKey() +"\n"+ entry.getValue());	 
-	}
-	System.out.println(map);
-	System.out.println(map.size());
-	System.out.println(map.get("zhao"));
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		map.put("li", 1);
+		map.put("wang", 2);
+		map.put("sun", 3);
+		map.put("zhao", 4);
+		map.put("xie", 5);
+		map.remove("xie");
+		Iterator<Map.Entry<String, Integer>> entries = map.entrySet().iterator();
+		while (entries.hasNext()) {
+			Map.Entry<String, Integer> entry = entries.next();
+			System.out.println(entry.getKey() + "\n" + entry.getValue());
+		}
+		System.out.println(map);
+		System.out.println(map.size());
+		System.out.println(map.get("zhao"));
 	}
 }
--- src/main/java/com/sixe/Lesson4.java
+++ src/main/java/com/sixe/Lesson4.java
@@ -1,22 +1,24 @@
 package com.sixe;
+
 import java.util.HashMap;
+
 public class Lesson4 {
 	public static void main(String args[]) {
-	HashMap<String, Integer> map = new HashMap<String, Integer>();
-	map.put("li",1);
-	map.put("wang",2);
-	map.put("sun",3);
-	map.put("zhao",4);
-	map.put("xie",5);
-	map.remove("sun");
-	for (String key : map.keySet()) {	 
-	    System.out.println(key); 
-	}
-	for (Integer value : map.values()) {
-	    System.out.println(value);
-	}
-	System.out.println(map);
-	System.out.println(map.size());
-	System.out.println(map.get("zhao"));
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		map.put("li", 1);
+		map.put("wang", 2);
+		map.put("sun", 3);
+		map.put("zhao", 4);
+		map.put("xie", 5);
+		map.remove("sun");
+		for (String key : map.keySet()) {
+			System.out.println(key);
+		}
+		for (Integer value : map.values()) {
+			System.out.println(value);
+		}
+		System.out.println(map);
+		System.out.println(map.size());
+		System.out.println(map.get("zhao"));
 	}
 }
